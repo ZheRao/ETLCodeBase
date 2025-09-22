87177f96d323762472c4fa47e2586157fc81e2f6
Zhe Rao <zheraoeminem@gmail.com>
2025-09-22 09:34:39 -0600

    ETL Stablized. HP, more QBO data extraction in progress

--- scripts/ETL.py
+++ scripts/ETL.py
@@ -4,10 +4,10 @@ from ETLCodeBase import JobClasses
 def main():
     # Extract and Transform
     QBOjob = JobClasses.QBOETL()
-    QBOjob.run(QBO_light=True, extract=True)
+    QBOjob.run(QBO_light=True, extract=False)
 
-    QBOTimeJob = JobClasses.QBOTimeETL()
-    QBOTimeJob.run(force_run=False)
+    # QBOTimeJob = JobClasses.QBOTimeETL()
+    # QBOTimeJob.run(force_run=False)
 
     # final transform
     # projects = JobClasses.Projects()
